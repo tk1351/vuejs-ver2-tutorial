e81ba949ff23bdb0a20e2be3b4913d36affbc219
tk1351 <takakikitsui0111@gmail.com>
2021-12-25 09:32:21 +0900

    feat: 5.2.3まで

--- app.js
+++ app.js
@@ -1,4 +1,36 @@
+const TodoList = {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+        <template v-for="todo in todos">
+            <slot :todo="todo">
+                <li :key="todo.id">
+                {{todo.text}}
+              </li>
+            </slot>
+        </template>
+    </ul>
+  `
+}
+
 new Vue({
-  el: "#app",
-  data: function () {},
-});
+  el: '#app',
+  components: {
+    TodoList,
+  },
+  data: function () {
+    return {
+      todos: [
+        { id: 1, text: 'C#', isCompleted: true },
+        { id: 2, text: 'React', isCompleted: false },
+        { id: 3, text: 'Java', isCompleted: true },
+        { id: 4, text: 'Perl', isCompleted: false },
+      ]
+    }
+  }
+})
